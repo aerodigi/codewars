b302b25b88f800e87664cce4c096431d96240841
Cenere Aereo <54714520+aerodigi@users.noreply.github.com>
2023-06-02 19:00:43 +0100

    AERO1 - Convert string to an array

---
+++ Convert_String_To_An_Array.py
@@ -0,0 +1,9 @@
+"""Write a function to split a string and convert it into an array of words."""
+
+def string_to_array(s):
+    split_list = s.split(" ")
+    return split_list
+
+
+
+string_to_array("Ainsley Ashley")
